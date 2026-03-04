72efe27d35decd7578dbcc370af5ea678cf36031
Bryan Lucas <stodutolucas1@gmail.com>
2026-03-04 16:25:09 -0300

    Add files via upload

---
+++ dartpad.dart
@@ -0,0 +1,75 @@
+
+void main() {
+  print("Seja bem vindo irei lhe mostrar seus dados.");
+  
+  var seunome = "Seu nome é: Bryan";
+  print(seunome);
+  
+  var seusobrenome = "Seu sobrenome é: Lucas";
+  print(seusobrenome);
+  
+  
+  int idade = 19;
+  print(idade);
+  
+  String endereco = "Seu edereço é: Carlos Bier";
+    print(endereco);
+}
+
+
+void main() {
+  
+   print("Vamos passar os valores corretos");
+   
+var vlringresso=20;
+  print(vlringresso);
+
+var resultado =(vlringresso/4);
+  print(resultado);
+}
+
+
+void main() {
+  
+ print("vamos para os resultados");
+
+var bmaior=10;
+  print(bmaior);
+
+var bmenor=2;
+  print(bmenor);
+
+var h = 3;
+  print(h);
+
+var area =(bmaior+bmenor*h /2);
+  print(area);
+  
+print("O valor da área desse trapézio é de 10 a 13");
+}
+
+
+void main(){
+  
+  print("Resultado: ");
+    
+  var  calcularArea = ("lado");
+  print(calcularArea);
+  
+  var calcularPerimetro = ("4 lados");
+  print (calcularPerimetro);
+   
+  var lado = ("lado");
+  print(lado);
+  
+  
+ 
+  
+    print ("O quadrado de lado: "
+                +lado+"\n Área: "+ calcularArea);
+  
+  print ("\nPerimetro: "+calcularPerimetro);
+                
+
+  
+}
